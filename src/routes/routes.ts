79937e51bb5d9312f178759cb9d0caa22dfad37e
Valentin Ratov <valentyn.ratov@ua-mac-039.local>
2020-09-10 19:46:33 +0300

    Clear code

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,3 @@
-// const prefixServerUrl: string = 'https://platform';
-
-// const hostReg: RegExpMatchArray | null = window.location.hostname.match('\\..+');
-// const hostUrl: string = hostReg ? hostReg[0] : '.wizplaydev.com';
-
-// const serverUrl: string = `${prefixServerUrl}${hostUrl}`;
 const serverUrl: string = `http://localhost:3000`;
 
 const graphqlPath: string = '/graphql';
